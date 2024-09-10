bbe9a830630d4b87f6105fbd852ac4f50d8aaf5a
Neidys Vargas <neidys.vargas@nxp.com>
2024-09-10 15:27:22 -0600

    headers

--- generated/guider_fonts/lv_font_Adventpro_regular_30.c
+++ generated/guider_fonts/lv_font_Adventpro_regular_30.c
@@ -1,11 +1,10 @@
 /*
- * Copyright 2024 NXP
- * NXP Proprietary. This software is owned or controlled by NXP and may only be used strictly in
- * accordance with the applicable license terms. By expressly accepting such terms or by downloading, installing,
- * activating and/or otherwise using the software, you are agreeing that you have read, and that you agree to
- * comply with and are bound by, such license terms.  If you do not agree to be bound by the applicable license
- * terms, then you may not retain, install, activate or otherwise use the software.
- */
+* Copyright (c) 2011, Andreas Kalpakides (hello@inderesting.com),
+* with Reserved Font Name “Advent Pro”.
+* All rights reserved.
+*
+* SPDX-License-Identifier: SIL OPEN FONT LICENSE Version 1.1 - 26 February 2007
+*/
 /*******************************************************************************
  * Size: 30 px
  * Bpp: 4
--- generated/guider_fonts/lv_font_montserratMedium_11.c
+++ generated/guider_fonts/lv_font_montserratMedium_11.c
@@ -1,11 +1,10 @@
 /*
- * Copyright 2024 NXP
- * NXP Proprietary. This software is owned or controlled by NXP and may only be used strictly in
- * accordance with the applicable license terms. By expressly accepting such terms or by downloading, installing,
- * activating and/or otherwise using the software, you are agreeing that you have read, and that you agree to
- * comply with and are bound by, such license terms.  If you do not agree to be bound by the applicable license
- * terms, then you may not retain, install, activate or otherwise use the software.
- */
+* Copyright 2011 The Montserrat Project Authors (
+https://github.com/JulietaUla/Montserrat)
+* All rights reserved.
+*
+* SPDX-License-Identifier: SIL OPEN FONT LICENSE Version 1.1 - 26 February 2007
+*/
 /*******************************************************************************
  * Size: 11 px
  * Bpp: 4
